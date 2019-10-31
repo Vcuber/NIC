29b86b79fea6cb98d5baabd5496f1dcdfc508aa4
Vcuber <31827964+Vcuber@users.noreply.github.com>
2019-10-31 19:59:39 +0530

    Add files via upload

--- crack.js
+++ crack.js
@@ -1,13 +1,3 @@
-function checkForm()
-{    
-    if(document.a.c.value == "Legion")
-    {
-     alert("Well Done! Now onto the final round.")
-     document.location.href="thefinale.html";   
-    }
-    else 
-    {
-        alert("Try again :P");
-        document.location.href="crack.html";
-    }
-}
+function checkForm(){/*WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOPS*/
+/*You're looking in the wrong place */    
+"Legion"==document.a.c.value?(alert("Well Done! Onto the next :')"),document.location.href="lev.html"):(alert/*Or Are you?*/("Try again :P"),document.location.href="crack.html")}
--- psswd.js
+++ psswd.js
@@ -1,10 +1,1 @@
-var s;
-s = prompt("Password kodi plis: ");
-if(s=="IFoundIt")
-{
-    alert("Well played. Off to the next one");
-    window.location.href="wartc.html";
-}
-else {
-    alert("Looks like you still haven't learnt anything. *facepalm*");
-}
+var s;"IFoundIt"==(s=prompt("Password kodi plis: "))?(alert("Well played. Off to the next one"),window.location.href="wartc.html"):alert("Looks like you still haven't learnt anything. *facepalm*");
