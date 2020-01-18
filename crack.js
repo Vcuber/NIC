9512d10f2ff6ff0c1933a8539dd16b4e36e26c20
Vcuber <31827964+Vcuber@users.noreply.github.com>
2020-01-18 10:36:43 +0530

    Add files via upload

--- crack.js
+++ crack.js
@@ -1,3 +1,3 @@
 function checkForm(){/*WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOPS*/
 /*You're looking in the wrong place */    
-"Legion"==document.a.c.value?(alert("Well Done! Onto the next :')"),document.location.href="lev.html"):(alert/*Or Are you?*/("Try again :P"),document.location.href="crack.html")}
+"Legion"==document.a.c.value?(alert("Well Done! Now onto the final round."),document.location.href="lev.html"):(alert/*Or Are you?*/("Try again :P"),document.location.href="crack.html")}
